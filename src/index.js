4c1776b1a89e10f263e2a09375f008a6e2e171b1
Mukul Singh <dakshmukul15@gmail.com>
2024-03-15 17:26:21 +0530

    navigation fix

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
+        path: "/",
+        element: <Feed />,
+      },
+      {
         path: "/feed",
         element: <Feed />,
       },
